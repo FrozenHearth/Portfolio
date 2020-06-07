a44f5f540c8ebb5d95296bd88516f69b422b7ec0
Vishwanath Sastry <vishu.dbz@gmail.com>
2020-06-07 19:57:04 +0530

    Scroll to the specific component on nav item click

---
+++ src/components/Projects.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Projects = () => {
+  return (
+    <div
+      id="scrollTo"
+      style={{
+        background: 'blue',
+        border: '1px solid grey',
+        width: 300,
+        height: 300,
+        marginTop: '400px',
+        marginBottom: '200px'
+      }}
+    >
+      <h1>This is a project.</h1>
+    </div>
+  );
+};
+
+export default Projects;
