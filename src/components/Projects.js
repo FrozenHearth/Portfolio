7d57ac122c96a66800e99930a83f104fcf0156bc
Vishwanath Sastry <vishu.dbz@gmail.com>
2020-06-10 02:10:39 +0530

    Integrate Github API to show specific projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Container } from '@material-ui/core';
 import ProjectCards from './ProjectCards';
+import axios from 'axios';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -18,12 +19,52 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
+const getToken = async () => {
+  try {
+    const res = await axios.get(process.env.REACT_APP_PROD);
+    return res.data.accessToken;
+  } catch (err) {
+    return console.log(err);
+  }
+};
+
+const getRepos = token => {
+  return axios
+    .get('https://api.github.com/users/FrozenHearth/repos', {
+      headers: {
+        Authorization: `token ${token}`
+      }
+    })
+    .then(res => {
+      const filteredRepos = res.data.filter(
+        repo => !repo.fork && repo.stargazers_count > 0 && !repo.private
+      );
+      return filteredRepos;
+    });
+};
+
 const Projects = () => {
   const classes = useStyles();
+  const [token, setAccessToken] = useState('');
+  const [repos, setRepos] = useState([]);
+
+  useEffect(() => {
+    getToken().then(accessToken => {
+      setAccessToken(accessToken);
+    });
+  }, [token]);
+  useEffect(() => {
+    if (token) {
+      getRepos(token).then(filteredRepos => {
+        setRepos(filteredRepos);
+      });
+    }
+  }, [token]);
+
   return (
     <Container size="md" id="scrollTo" className={classes.root}>
       <h1 className={classes.title}>Projects</h1>
-      <ProjectCards />
+      {repos.length > 0 ? <ProjectCards repos={repos} /> : ''}
     </Container>
   );
 };
